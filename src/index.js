b118df51c4d0551765e613390c63a5bb2835d196
Muhammad-Faisal_FTA <mfaisal1947fort@gmail.com>
2026-03-01 13:58:24 +0500

    feat(ws): web socket server is set and active.

--- src/index.js
+++ src/index.js
@@ -1,35 +1,23 @@
 import http from "http";
-import { db } from "./db/db.js";
+import express from "express";
+// import { db } from "./db/db.js";
 import { createUser, getAllUsers } from "./queries.js";
 
 const PORT = 4000;
 
+const app = express();
 
+app.use(express.json());
 /* ---------------- HTTP Server ---------------- */
 
-const server = http.createServer(async (req, res) => {
-  try {
-    // Test database connection
-    const result = await db.execute('SELECT NOW()');
-    console.log('Database connection successful:', result);
+const server = http.createServer(app)
+app.get("/users",getAllUsers);
+app.post("/users",createUser);
+ 
+const { broadCastStarted }  = attachWebSocketServer(server);
 
-    // Example: Create a test user
-    const testUser = await createUser('Test User', 'test@example.com');
-    console.log('Test user created:', testUser);
-
-    // Example: Get all users
-    const allUsers = await getAllUsers();
-    console.log('All users:', allUsers);
-
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("HTTP Server running on port 4000 with Drizzle + Neon connection. Test user created and retrieved.");
-  } catch (error) {
-    console.error('Database connection failed:', error);
-    res.writeHead(500, { "Content-Type": "text/plain" });
-    res.end("HTTP Server running on port 4000, but database connection failed");
-  }
-});
 
 server.listen(PORT, () => {
+  console.log(`Websocket is runing on: ${PORT}/ws`)
   console.log(`Server listening on port ${PORT}`);
 });
---
+++ src/ws/server.js
@@ -0,0 +1,42 @@
+import WebSocket from 'ws'; 
+function sendJson(websocket, data){
+    if(websocket.readyState != websocket.OPEN){
+        console.log("WebSocket is not open");
+        return;
+    }
+    websocket.send(JSON.stringify(data));
+}
+
+function broadCast(wss, data){
+    for(const client of wss.clients){
+        if(client.readyState != WebSocket.OPEN){
+            return;
+    }
+
+    client.send(JSON.stringify(data));
+}}
+
+export function attachWebSocketServer(server){
+    const wss = new WebSocket.Server({ 
+        server,
+        path: '/ws',
+        maxPayload: 1024 * 1024, // 1MB 
+     });
+
+    server.on('connection', (ws) => {
+        sendJson(ws, { message: 'Welcome to the WebSocket server!' });
+        console.log('WebSocket client connected');
+    })
+
+    server.on('error', error => {
+        console.error('WebSocket error:', error.message);
+    })
+
+    function broadCastStarted(match){
+    broadCast(wss, { event: 'matchStarted', match });
+    }
+
+    return { broadCastStarted: broadCastStarted };
+
+
+}
